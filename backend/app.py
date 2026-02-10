6684790135df10d1435869f996e09d391805e1c3
Omkar Chebale <omkarchebale0@gmail.com>
2026-02-11 01:35:14 +0530

    fix(cors): update cors setting

--- backend/app.py
+++ backend/app.py
@@ -102,7 +102,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://narrative-transformation-pipeline.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
